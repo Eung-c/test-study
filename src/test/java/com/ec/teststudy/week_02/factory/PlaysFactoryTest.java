4094739406d996596b53426c71c5b6e27766ffd6
jaehyun <vljh246v@naver.com>
2020-05-20 21:11:17 +0900

    PlaysFactory 테스트 추가

--- src/test/java/com/ec/teststudy/week_02/factory/PlaysFactoryTest.java
+++ src/test/java/com/ec/teststudy/week_02/factory/PlaysFactoryTest.java
@@ -1,7 +1,10 @@
 package com.ec.teststudy.week_02.factory;
 
+import com.ec.teststudy.week_02.domain.Performance;
+import com.ec.teststudy.week_02.domain.Plays;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class PlaysFactoryTest {
@@ -14,5 +17,23 @@ public class PlaysFactoryTest {
         playsFactory = new PlaysFactory();
         assertNotNull(playsFactory);
 
+    }
+
+    @Test
+    public void createPlay(){
+        // given
+        Performance performance = new Performance("as-like", 35);
+        Plays expected = new PlaysFactory().getFor(performance.getPlayId());
+        String expectedName = "As You Like It";
+
+        assertThat(expectedName, is(expected.getPlaysInfo().getName()));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createWrongPlay(){
+        // given
+        Performance performance = new Performance("wrong-play", 35);
+        Plays expected = new PlaysFactory().getFor(performance.getPlayId());
     }
 }
