6bfb0b194920a15dc9d6a6f0dc55e1ccee39dad3
jaehyun <vljh246v@naver.com>
2020-05-28 01:55:26 +0900

    3주차 업로드

---
+++ src/main/java/com/ec/teststudy/week_03/6.8/introduceParameterObject.js
@@ -0,0 +1,7 @@
+function amountInvoiced_before(startData, endDate){/* */}
+function amountReceived_before(startData, endDate){/* */}
+function amountOverdue_before(startData, endDate){/* */}
+
+function amountInvoiced_after(aDateRange){/* */}
+function amountReceived_after(aDateRange){/* */}
+function amountOverdue_after(aDateRange){/* */}
